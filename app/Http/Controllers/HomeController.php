616f1550fce5b2c00b74b61541a0074995cbd6bd
shoxjaxon <shohjahonsobirjonov76@gmail.com>
2025-06-18 14:07:40 +0500

    Men Laravel Breezeni o'rnatib hozir saytni layoutslarga bo'lyapman

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        $usertype = Auth()->user()->usertype;
+
+        if($usertype == 'user'){
+
+            return to_route('dashboard');
+        }elseif($usertype == 'admin'){
+
+            return 'Salom Admin Panelga xush kelibsiz';
+        }else{
+
+            return redirect()->back();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//HomeController 
+Route::get('/ksfjfnvkdkwe5485rnfk/home',[HomeController::class, 'index'])->name('home.page');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
